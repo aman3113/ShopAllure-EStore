dfa8f4eb9957fbc5f41dfe72dfbbdf9c914e1693
AMAN <antilaman3113@gmail.com>
2023-07-21 21:44:36 +0530

    fix: address margin

--- shopallure/src/Pages/User/AddressForm.js
+++ shopallure/src/Pages/User/AddressForm.js
@@ -3,164 +3,164 @@ import { useDispatch, useSelector } from "react-redux";
 import { setAddress } from "../../Redux/UserSlice";
 
 const AddressForm = ({ setShowForm, editIndex, editForm, setEditForm }) => {
-  const addressArr = useSelector((store) => store.user.address);
-  const [formDetails, setFormDetails] = useState(
-    editForm
-      ? addressArr[editIndex]
-      : {
-          name: "",
-          location: "",
-          city: "",
-          state: "",
-          country: "",
-          pinCode: "",
-          phoneNumber: "",
-        }
-  );
-  const dispatch = useDispatch();
+	const addressArr = useSelector((store) => store.user.address);
+	const [formDetails, setFormDetails] = useState(
+		editForm
+			? addressArr[editIndex]
+			: {
+					name: "",
+					location: "",
+					city: "",
+					state: "",
+					country: "",
+					pinCode: "",
+					phoneNumber: "",
+			  }
+	);
+	const dispatch = useDispatch();
 
-  function handleEditedChanges(e) {
-    e.preventDefault();
-    const editedAddressArr = addressArr.map((address, idx) => {
-      if (editIndex === idx) {
-        return formDetails;
-      }
-      return address;
-    });
-    dispatch(setAddress(editedAddressArr));
-    setShowForm(false);
-    setEditForm(false);
-  }
+	function handleEditedChanges(e) {
+		e.preventDefault();
+		const editedAddressArr = addressArr.map((address, idx) => {
+			if (editIndex === idx) {
+				return formDetails;
+			}
+			return address;
+		});
+		dispatch(setAddress(editedAddressArr));
+		setShowForm(false);
+		setEditForm(false);
+	}
 
-  function handleSave(e) {
-    e.preventDefault();
-    dispatch(setAddress([...addressArr, formDetails]));
-    setShowForm(false);
-    setEditForm(false);
-  }
+	function handleSave(e) {
+		e.preventDefault();
+		dispatch(setAddress([...addressArr, formDetails]));
+		setShowForm(false);
+		setEditForm(false);
+	}
 
-  function handleCancel(e) {
-    e.preventDefault();
-    setShowForm(false);
-    setEditForm(false);
-  }
+	function handleCancel(e) {
+		e.preventDefault();
+		setShowForm(false);
+		setEditForm(false);
+	}
 
-  function handleDummyData(e) {
-    e.preventDefault();
-    setFormDetails({
-      name: "Antil Aman",
-      location: "sec-14 MG-Road",
-      city: "Gurgaon",
-      state: "Haryana",
-      country: "India",
-      pinCode: "110039",
-      phoneNumber: "8897456780",
-    });
-  }
+	function handleDummyData(e) {
+		e.preventDefault();
+		setFormDetails({
+			name: "Antil Aman",
+			location: "sec-14 MG-Road",
+			city: "Gurgaon",
+			state: "Haryana",
+			country: "India",
+			pinCode: "110039",
+			phoneNumber: "8897456780",
+		});
+	}
 
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setFormDetails((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  }
+	function handleChange(e) {
+		const { name, value } = e.target;
+		setFormDetails((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	}
 
-  return (
-    <div className="w-[90%] md:w-[60%] lg:w-[35%] border border-black py-3 my-4 px-8 shadow-md shadow-purple-300 rounded-md">
-      <p className="font-bold text-center text-lg m-3">Add New Address</p>
-      <form className="flex flex-col">
-        <input
-          className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
-          type="text"
-          placeholder="Enter Name"
-          name="name"
-          value={formDetails.name}
-          onChange={handleChange}
-        />
-        <input
-          className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
-          type="text"
-          placeholder="Enter House no., colony, street"
-          name="location"
-          value={formDetails.location}
-          onChange={handleChange}
-        />
-        <input
-          className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
-          type="text"
-          placeholder="Enter city"
-          name="city"
-          value={formDetails.city}
-          onChange={handleChange}
-        />
-        <input
-          className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
-          type="text"
-          placeholder="Enter state"
-          name="state"
-          value={formDetails.state}
-          onChange={handleChange}
-        />
-        <input
-          className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
-          type="text"
-          placeholder="Enter country"
-          name="country"
-          value={formDetails.country}
-          onChange={handleChange}
-        />
-        <input
-          className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
-          type="number"
-          placeholder="Enter PinCode"
-          name="pinCode"
-          value={formDetails.pinCode}
-          onChange={handleChange}
-        />
-        <input
-          className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
-          type="number"
-          placeholder="Enter Mobile Number"
-          name="phoneNumber"
-          value={formDetails.phoneNumber}
-          onChange={handleChange}
-        />
-        <div className="flex justify-around my-3 gap-1 md:m-3">
-          {editForm ? (
-            <button
-              className="border bg-blue-700 text-white px-3 py-1 rounded-lg"
-              onClick={handleEditedChanges}
-            >
-              Save Changes
-            </button>
-          ) : (
-            <button
-              className="border bg-blue-700 text-white px-3 py-1 rounded-lg"
-              onClick={handleSave}
-            >
-              Save
-            </button>
-          )}
+	return (
+		<div className="w-[90%] md:w-[60%] lg:w-[35%] border border-black py-3 my-4 px-8 shadow-md shadow-purple-300 rounded-md">
+			<p className="font-bold text-center text-lg m-3">Add New Address</p>
+			<form className="flex flex-col">
+				<input
+					className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
+					type="text"
+					placeholder="Enter Name"
+					name="name"
+					value={formDetails.name}
+					onChange={handleChange}
+				/>
+				<input
+					className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
+					type="text"
+					placeholder="Enter House no., colony, street"
+					name="location"
+					value={formDetails.location}
+					onChange={handleChange}
+				/>
+				<input
+					className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
+					type="text"
+					placeholder="Enter city"
+					name="city"
+					value={formDetails.city}
+					onChange={handleChange}
+				/>
+				<input
+					className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
+					type="text"
+					placeholder="Enter state"
+					name="state"
+					value={formDetails.state}
+					onChange={handleChange}
+				/>
+				<input
+					className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
+					type="text"
+					placeholder="Enter country"
+					name="country"
+					value={formDetails.country}
+					onChange={handleChange}
+				/>
+				<input
+					className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
+					type="number"
+					placeholder="Enter PinCode"
+					name="pinCode"
+					value={formDetails.pinCode}
+					onChange={handleChange}
+				/>
+				<input
+					className="border  border-gray-400 text-sm  p-1 my-2 hover:border-pink-300 rounded-md"
+					type="number"
+					placeholder="Enter Mobile Number"
+					name="phoneNumber"
+					value={formDetails.phoneNumber}
+					onChange={handleChange}
+				/>
+				<div className="flex justify-around my-3 gap-1 md:m-3">
+					{editForm ? (
+						<button
+							className="border bg-blue-700 text-white px-3 py-1 rounded-lg"
+							onClick={handleEditedChanges}
+						>
+							Save Changes
+						</button>
+					) : (
+						<button
+							className="border bg-blue-700 text-white px-3 py-1 rounded-lg"
+							onClick={handleSave}
+						>
+							Save
+						</button>
+					)}
 
-          <button
-            className="border bg-red-500 text-white px-3 py-1 rounded-lg"
-            onClick={handleCancel}
-          >
-            Close
-          </button>
-          {!editForm && (
-            <button
-              className="border bg-purple-700 text-white px-3 py-1 rounded-lg"
-              onClick={handleDummyData}
-            >
-              Fill Dummy Data
-            </button>
-          )}
-        </div>
-      </form>
-    </div>
-  );
+					<button
+						className="border bg-red-500 text-white px-3 py-1 rounded-lg"
+						onClick={handleCancel}
+					>
+						Close
+					</button>
+					{!editForm && (
+						<button
+							className="border bg-purple-700 text-white px-3 py-1 rounded-lg"
+							onClick={handleDummyData}
+						>
+							Fill Dummy Data
+						</button>
+					)}
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default AddressForm;
--- shopallure/src/Pages/User/User.js
+++ shopallure/src/Pages/User/User.js
@@ -5,132 +5,132 @@ import { setAddress } from "../../Redux/UserSlice";
 import { toast } from "react-toastify";
 
 const User = () => {
-  const [details, setDetails] = useState("profile");
-  const [editForm, setEditForm] = useState(false);
-  const [editIndex, setEditIndex] = useState(null);
-  const [showForm, setShowForm] = useState(false);
-  const dispatch = useDispatch();
+	const [details, setDetails] = useState("profile");
+	const [editForm, setEditForm] = useState(false);
+	const [editIndex, setEditIndex] = useState(null);
+	const [showForm, setShowForm] = useState(false);
+	const dispatch = useDispatch();
 
-  const { firstName, lastName, email } = useSelector(
-    (store) => store.user.profile
-  );
-  const addressArr = useSelector((store) => store.user.address);
+	const { firstName, lastName, email } = useSelector(
+		(store) => store.user.profile
+	);
+	const addressArr = useSelector((store) => store.user.address);
 
-  function handleEdit(idx) {
-    setEditForm(true);
-    setShowForm(true);
-    setEditIndex(idx);
-  }
+	function handleEdit(idx) {
+		setEditForm(true);
+		setShowForm(true);
+		setEditIndex(idx);
+	}
 
-  function handleRemove(idx) {
-    const newAddressArr = addressArr.filter((address, index) => index !== idx);
-    console.log(newAddressArr);
-    dispatch(setAddress(newAddressArr));
-  }
+	function handleRemove(idx) {
+		const newAddressArr = addressArr.filter((address, index) => index !== idx);
+		console.log(newAddressArr);
+		dispatch(setAddress(newAddressArr));
+	}
 
-  function handleClick() {
-    localStorage.removeItem("encodedToken");
-    localStorage.removeItem("eCommerceLoggedIn");
-    sessionStorage.removeItem("encodedToken");
-    sessionStorage.removeItem("eCommerceLoggedIn");
-    sessionStorage.removeItem("user");
-    sessionStorage.removeItem("user");
-    toast("You are logged out");
-    window.location.reload();
-  }
-  return (
-    <div className="min-h-[70vh] flex items-center justify-center ">
-      {showForm ? (
-        <AddressForm
-          setShowForm={setShowForm}
-          setEditForm={setEditForm}
-          editIndex={editIndex}
-          editForm={editForm}
-        />
-      ) : (
-        <div className="w-[80%] md:w-[60%] border-2 border-black p-1 md:p-3 shadow-md shadow-purple-300 rounded-md">
-          <h1 className="text-center font-bold text-xl m-4">Account</h1>
-          <div>
-            <div className=" flex ">
-              <span
-                className={`w-[50%] text-center cursor-pointer ${
-                  details === "profile" &&
-                  "bg-purple-400 border-b-2 border-purple-900"
-                }`}
-                onClick={() => setDetails("profile")}
-              >
-                Profile
-              </span>
-              <span
-                className={`w-[50%] text-center cursor-pointer ${
-                  details === "address" &&
-                  "bg-purple-400 border-b-2  border-purple-900"
-                }`}
-                onClick={() => setDetails("address")}
-              >
-                Address
-              </span>
-            </div>
-            {details === "profile" && (
-              <div className="p-2">
-                <p className="font-bold mb-3">Profile Details</p>
-                <p>
-                  <span className="font-semibold">Full Name :</span> {firstName}{" "}
-                  {lastName}
-                </p>
-                <p>
-                  {" "}
-                  <span className="font-semibold">Email:</span> {email}
-                </p>
-                <button
-                  onClick={handleClick}
-                  className="bg-red-600 text-white py-1 px-2 rounded-lg mt-3"
-                >
-                  LogOut
-                </button>
-              </div>
-            )}
-            {details === "address" && (
-              <div className="p-2">
-                <p className="font-bold underline mb-4">My Addresses</p>
-                {addressArr.map((address, idx) => (
-                  <div key={idx} className="bg-slate-100 p-2 m-1">
-                    <p className="font-semibold">{address.name}</p>
-                    <p>{address.location}</p>
-                    <p>{address.pinCode}</p>
-                    <p>
-                      {address.city}, {address.state}
-                    </p>
-                    <p>{address.country}</p>
-                    <div className="mt-1">
-                      <button
-                        className="border text-blue-800 bg-white py-1 mr-3 px-2 rounded-lg font-semibold"
-                        onClick={() => handleEdit(idx)}
-                      >
-                        Edit
-                      </button>
-                      <button
-                        className="border text-red-500 bg-white py-1 px-2 rounded-lg font-semibold"
-                        onClick={() => handleRemove(idx)}
-                      >
-                        Remove
-                      </button>
-                    </div>
-                  </div>
-                ))}
-                <button
-                  className="font-bold mt-3"
-                  onClick={() => setShowForm((prev) => !prev)}
-                >
-                  + Add new address
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
-      )}
-    </div>
-  );
+	function handleClick() {
+		localStorage.removeItem("encodedToken");
+		localStorage.removeItem("eCommerceLoggedIn");
+		sessionStorage.removeItem("encodedToken");
+		sessionStorage.removeItem("eCommerceLoggedIn");
+		sessionStorage.removeItem("user");
+		sessionStorage.removeItem("user");
+		toast("You are logged out");
+		window.location.reload();
+	}
+	return (
+		<div className="min-h-[70vh] flex items-center justify-center ">
+			{showForm ? (
+				<AddressForm
+					setShowForm={setShowForm}
+					setEditForm={setEditForm}
+					editIndex={editIndex}
+					editForm={editForm}
+				/>
+			) : (
+				<div className="w-[80%] md:w-[60%] border-2 border-black p-1 md:p-3 shadow-md shadow-purple-300 rounded-md my-3">
+					<h1 className="text-center font-bold text-xl m-4">Account</h1>
+					<div>
+						<div className=" flex ">
+							<span
+								className={`w-[50%] text-center cursor-pointer ${
+									details === "profile" &&
+									"bg-purple-400 border-b-2 border-purple-900"
+								}`}
+								onClick={() => setDetails("profile")}
+							>
+								Profile
+							</span>
+							<span
+								className={`w-[50%] text-center cursor-pointer ${
+									details === "address" &&
+									"bg-purple-400 border-b-2  border-purple-900"
+								}`}
+								onClick={() => setDetails("address")}
+							>
+								Address
+							</span>
+						</div>
+						{details === "profile" && (
+							<div className="p-2">
+								<p className="font-bold mb-3">Profile Details</p>
+								<p>
+									<span className="font-semibold">Full Name :</span> {firstName}{" "}
+									{lastName}
+								</p>
+								<p>
+									{" "}
+									<span className="font-semibold">Email:</span> {email}
+								</p>
+								<button
+									onClick={handleClick}
+									className="bg-red-600 text-white py-1 px-2 rounded-lg mt-3"
+								>
+									LogOut
+								</button>
+							</div>
+						)}
+						{details === "address" && (
+							<div className="p-2">
+								<p className="font-bold underline mb-4">My Addresses</p>
+								{addressArr.map((address, idx) => (
+									<div key={idx} className="bg-slate-100 p-2 m-1">
+										<p className="font-semibold">{address.name}</p>
+										<p>{address.location}</p>
+										<p>{address.pinCode}</p>
+										<p>
+											{address.city}, {address.state}
+										</p>
+										<p>{address.country}</p>
+										<div className="mt-1">
+											<button
+												className="border text-blue-800 bg-white py-1 mr-3 px-2 rounded-lg font-semibold"
+												onClick={() => handleEdit(idx)}
+											>
+												Edit
+											</button>
+											<button
+												className="border text-red-500 bg-white py-1 px-2 rounded-lg font-semibold"
+												onClick={() => handleRemove(idx)}
+											>
+												Remove
+											</button>
+										</div>
+									</div>
+								))}
+								<button
+									className="font-bold mt-3"
+									onClick={() => setShowForm((prev) => !prev)}
+								>
+									+ Add new address
+								</button>
+							</div>
+						)}
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default User;
